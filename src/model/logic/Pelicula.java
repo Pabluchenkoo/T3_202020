402cb0667ce9d28899786a308d1fcc053b587da3
Juan Pablo Junco <Pabluchenko@Juans-iMac.local>
2020-08-25 19:59:02 -0500

    classes

---
+++ src/model/logic/Pelicula.java
@@ -0,0 +1,33 @@
+package model.logic;
+
+import java.sql.Date;
+
+public class Pelicula 
+{
+	private int iD;
+	private int presupuesto;
+	private String imdb_Id;
+	private String idiomaOriginal;
+	private String tituloOriginal;
+	private String resumen;
+	private String popularidad;
+	private String productora;
+	private Date fechaEstreno;
+	private int ganancia;
+	private int tiempoReproduccion;
+	private String idiomasHablados;
+	private String Estado;
+	private String tagline;
+	private double promedioVotos;
+	private int cuentaVotos;
+	private int numeroCompañiaProductora;
+	private int numeroCompañiaPaises;
+	private int numeroIdiomasHablados;
+	
+	
+	
+	
+	
+	
+
+}
