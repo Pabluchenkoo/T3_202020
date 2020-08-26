19eb737d6e87f85b4d2098b8a6fab0181b91045c
Juan Felipe Caraballo <j.caraballo@uniandes.edu.co>
2020-08-25 21:09:37 -0500

    Agregados los metodos a la clase Pelicula

--- src/model/logic/Pelicula.java
+++ src/model/logic/Pelicula.java
@@ -20,14 +20,153 @@ public class Pelicula
 	private String tagline;
 	private double promedioVotos;
 	private int cuentaVotos;
-	private int numeroCompañiaProductora;
-	private int numeroCompañiaPaises;
+	private int numeroCompaniaProductora;
+	private int numeroCompaniaPaises;
 	private int numeroIdiomasHablados;
 	
+	private int darId(){
+		return iD;
+	}
+	
+	private void cambiarId(int nuevoId){
+		iD = nuevoId;
+	}
+	
+	private int darPresupuesto(){
+		return presupuesto;
+	}
+	
+	private void cambiarPresupuesto(int nuevoPresupuesto){
+		presupuesto = nuevoPresupuesto;
+	}
+	
+	private String darImdbId(){
+		return imdb_Id;
+	}
+	
+	private void cambiarImdbId(String nuevoId){
+		imdb_Id = nuevoId;
+	}
+	
+	private String darIdiomaOriginal(){
+		return idiomaOriginal;
+	}
+	
+	private void cambiarIdiomaOriginal(String nuevoIdioma){
+		idiomaOriginal = nuevoIdioma;
+	}
+	
+	private String darResumen()
+	{
+		return resumen;
+	}
+	
+	private void cambiarResumen(String nuevoResumen){
+		resumen = nuevoResumen;
+	}
+	
+	private String darPopularidad(){
+		return popularidad;
+	}
+	
+	private void cambiarPopularidad(String nuevaPopularidad){
+		popularidad = nuevaPopularidad;
+	}
+	
+	private String darProductora(){
+		return productora;
+	}
+	
+	private void cambiarProductora(String nuevaProductora){
+		productora = nuevaProductora;
+	}
+	
+	private Date darFechaEstreno(){
+		return fechaEstreno;
+	}
+	
+	private void cambiarFechaEstreno(Date nuevaFecha){
+		fechaEstreno = nuevaFecha;
+	}
+	
+	private int darGanancia(){
+		return ganancia;
+	}
+	
+	private void cambiarGanancia(int nuevaGanancia){
+		ganancia = nuevaGanancia;
+	}
+	
+	private int darTiempoReproduccion(){
+		return tiempoReproduccion;
+	}
+	
+	private void cambiarTiempoReproduccion(int nuevoTiempo){
+		tiempoReproduccion = nuevoTiempo;
+	}
+	
+	private String darIdiomasHablados(){
+		return idiomasHablados;
+	}
+	
+	private void cambiarIdiomasHablados(String nuevosIdiomas){
+		idiomasHablados = nuevosIdiomas;
+	}
 	
 	
+	private String darEstado(){
+		return Estado;
+	}
 	
+	private void cambiarEstado(String nuevoEstado){
+		Estado = nuevoEstado;
+	}
 	
+	private String darTagline(){
+		return tagline;
+	}
 	
-
+	private void cambiarTagline(String nuevoTagline){
+		tagline = nuevoTagline;
+	}
+	
+	private double darPromedioVotos(){
+		return promedioVotos;
+	}
+	
+	private void cambiarPromedioVotos(double nuevoPromedio){
+		promedioVotos =  nuevoPromedio;
+	}
+	
+	private int darCuentaVotos(){
+		return cuentaVotos;
+	}
+	
+	private void cambiarCuentaVotos(int nuevoCuenta){
+		cuentaVotos = nuevoCuenta;
+	}
+	
+	private int darNumeroCompaniaProductora(){
+		return numeroCompaniaProductora;
+	}
+	
+	private void cambiaNumeroCompaniaProductora(int nuevoN){
+		numeroCompaniaProductora = nuevoN;
+	}
+	
+	private int darNumeroCompaniaPaises(){
+		return numeroCompaniaPaises;
+	}
+	
+	private void cambiarNumeroCompaniaPaises(int nuevoN){
+		numeroCompaniaPaises = nuevoN;
+	}
+	
+	private int darNumeroIdiomasHablados(){
+		return numeroIdiomasHablados;
+	}
+	
+	private void cambiarNumeroIdiomasHablados(int nuevoN){
+		numeroIdiomasHablados = nuevoN;
+	}
 }
