23a3734e20a0d3439ed1934f4e6ace0632cf8f2e
Juan Pablo Junco <Pabluchenko@Juans-iMac.local>
2020-08-25 20:58:45 -0500

    casting

---
+++ src/model/logic/Casting.java
@@ -0,0 +1,26 @@
+package model.logic;
+
+public class Casting {
+	
+	private int iD;
+	private String nombreActor1;
+	private String generoActor1;
+	private String nombreActor2;
+	private String generoActor2;
+	private String nombreActor3;
+	private String generoActor3;
+	private String nombreActor4;
+	private String generoActor4;
+	private String nombreActor5;
+	private String generoActor5;
+	private int numeroActor;
+	private String nombreDirector;
+	private int generoDirector;
+	private int numeroDirector;
+	private String nombreProductor;
+	private int numeroProductor;
+	private String nombreEditor;
+	
+
+
+}
