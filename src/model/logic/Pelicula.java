7199b6c241724ed63960e02b0585ec6053c6cc6a
Juan Pablo Junco <Pabluchenko@Juans-iMac.local>
2020-09-08 21:19:36 -0500

    peli

--- src/model/logic/Pelicula.java
+++ src/model/logic/Pelicula.java
@@ -74,11 +74,11 @@ public class Pelicula implements Comparable<Pelicula>
 	
 	
 
-<<<<<<< HEAD
+
+
+
 	public Pelicula (int pID, double pPresupuesto,String pGeneros, String pImdb_Id, String pIdiomaOriginal, 
-=======
-	public Pelicula (int pID, double pPresupuesto,String pgeneros, String pImdb_Id, String pIdiomaOriginal, 
->>>>>>> eaaedb61dbb50d84878e9dff1ff99469a4b0e171
+
 
 			String pTituloOriginal, String pResumen, String pPopularidad,String pProductora, String productCompanies,
 			String productionCountries,
