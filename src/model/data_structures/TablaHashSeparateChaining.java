57f19988d47f9db1bc3d421787a4cb93c28d458a
Juan Pablo Junco <Pabluchenko@Juans-iMac.local>
2020-09-21 11:41:15 -0500

    e

---
+++ src/model/data_structures/TablaHashSeparateChaining.java
@@ -0,0 +1,62 @@
+package model.data_structures;
+
+public class TablaHashSeparateChaining<K extends Comparable<K>, V extends Comparable <V>> implements TablaSimbolos <K, V>
+{
+
+	@Override
+	public void put(K element, V element1) 
+	{
+		
+		
+	}
+
+	@Override
+	public V get(K element) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(K element) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(K element) 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArregloDinamico<K> keySet() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArregloDinamico<V> valueSet() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
